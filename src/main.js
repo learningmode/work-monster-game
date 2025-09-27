a01b12df3be2b4753bea79223b80ecf9d953eec3
learningmode <learningmode151@gmail.com>
2025-09-28 05:03:36 +0530

    updating preload scenes and adding asset links

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import Phaser from "./lib/phaser";
+import Phaser from "./lib/phaser.js";
+import { PreloadScene } from "./scenes/preload-scene.js";
+import { SCENE_KEYS } from "./scenes/scene-keys.js";
+
 const game = new Phaser.Game({
     parent:'game-container',
 });
+
+game.scene.add(PreloadScene.name,PreloadScene);
+game.scene.start(SCENE_KEYS.PRELOAD_SCENE);
